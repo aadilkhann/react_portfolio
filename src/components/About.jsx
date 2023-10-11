b5d92365c5b15995752075c0d0c43754cbb14a59
Adil Khan <khan01970@gmail.com>
2023-10-11 21:05:21 +0530

    Responsive Design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="md:pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
-        <p className="text-xl mt-20">
+        <p className="text-xl md:mt-20 mt-8">
           Hello! I'm Adil Khan, a passionate and enthusiastic fresher with a strong foundation in web development and Android application development. I have a solid understanding of various technologies such as React.js, Node.js, Express.js, MySQL, NoSQL, and Android development.
           <br />
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -79,10 +79,10 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
-          <p className="py-6">These are the technologies I've worked with</p>
+          <p className="md:py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:gap-8 gap-3 text-center md:py-8 py-4 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
